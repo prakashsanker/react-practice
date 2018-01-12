f1b25f3a4f12e352fa6ace02971110f0665cd093
abi121 <abhishek@xelpmoc.in>
2018-01-12 19:59:00 +0530

    my first commit  for tic toc game

---
+++ src/square.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// class Square extends React.Component{
+//     // constructor(props){
+//     //     super(props);
+//     //     this.state={value:'',};
+//     // }
+//     render(){
+//         return(
+//         <button className="square" onClick={()=> this.props.onClick()}>
+//         {this.props.value}
+//         </button>
+//         );
+//     }
+// }
+function Square(props) {
+        return(
+        <button className="square" onClick={()=> props.onClick()}>
+        {props.value}
+        </button>
+        );
+}
+
+export default Square;
