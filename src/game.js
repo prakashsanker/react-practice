a483052dd70cf33397cb64f3bf4cdf5f62d7c884
InshaNazeer <insha@xelpmoc.in>
2018-01-16 12:19:44 +0530

    TicTacToe Program

---
+++ src/game.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Board from './board.js';
+export default class Game extends React.Component {
+    render() {
+      return (
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+          </div>
+        </div>
+      );
+    }
+  }
